12731798f6950dab6c6ac8eda5e3f5456aaeda35
gotimo2 <timost1402@hotmail.com>
2022-07-16 20:51:58 +0200

    dice improvements

---
+++ GMTK game jam 2022/Assets/DiceBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceBehaviour : MonoBehaviour
+{
+
+    public static GameObject playersDice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playersDice = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

bd3db5cd440caf70016b75298a63fabc6b0a9205
gotimo2 <timost1402@hotmail.com>
2022-07-16 19:52:59 +0200

    stabilizer cubes

--- GMTK game jam 2022/Assets/Scripts/StabilizerCubeBehaviour.cs
+++ GMTK game jam 2022/Assets/Scripts/StabilizerCubeBehaviour.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class StabilizerCubeBehaviour : MonoBehaviour
 {
-    public Animator Animator;
+    public Animator animator;
+
+    public bool activated;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +17,19 @@ public class StabilizerCubeBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (activated &&  !animator.GetCurrentAnimatorStateInfo(0).IsName("Turning_anim"))
+        {
+            animator.Play("Restabled_anim");
+        }
         
     }
 
     public void OnCollisionEnter2D(Collision2D collision2D)
     {
-        
+        if (collision2D.gameObject.CompareTag("Player"))
+        {
+            animator.Play("Turning_anim");
+        }
     }
 
     private void Awake()

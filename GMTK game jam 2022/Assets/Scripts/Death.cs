b3a90bb401e4b19a609e00adcc66df1da555d232
Aaron <aaronhampson24@gmail.com>
2022-07-16 21:18:02 +0100

    some level stuff (timo im vcommiting)
    only a few

---
+++ GMTK game jam 2022/Assets/Scripts/Death.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+        }
+    }
+}
